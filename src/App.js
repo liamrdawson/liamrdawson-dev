443833256d603579a4de5af160cfcc946f1c2e90
Liam Dawson <liamrdawson@gmail.com>
2019-08-16 23:13:32 +0100

    clean up html template, render elements to DOM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function Header() {
+    return (
+        <header>
+            <h1>
+            Hello World
+        </h1>
+        <h3>What are you looking at?</h3>
+        </header>
+    );
+}
 
+function App() {
+    return (
+        <Header />
+    );
 }
 
 export default App;
