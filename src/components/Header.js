268cf8cf0d34816121e3ce76b65aebee81114616
Liam Dawson <liamrdawson@gmail.com>
2019-08-27 21:56:00 +0100

    adjust nav item flexboxes and positioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,22 +16,28 @@ class Header extends Component {
         console.log(this.props);
         return (
             <header>
-                <div className="header-elements-container">
-                    <button onClick={this.props.clickHome}><span>Liam R. Dawson</span></button>
-
+                
                     {/* <span className="menu" onClick={this.toggleMenu}>Menu</span> */}
 
                     {this.state.showMenuIcon === false &&
-                        (<nav>
+                        (<div className="menu-active">
+                            <nav >
                             <a href="#">About</a>
                             <a href="#projects">Projects</a>
                             <button onClick={this.props.clickContact}>Contact</button>
                             <button onClick={this.toggleMenu}>Close</button>
-                        </nav> )
+                        </nav> 
+                        </div>)
                     }
-                    {this.state.showMenuIcon === true && (<button  onClick={this.toggleMenu}>Menu</button>)}
 
-                </div>
+                    {this.state.showMenuIcon === true && 
+                        (<div className="header-elements-container">
+                            <button onClick={this.props.clickHome}><span>Liam R. Dawson</span></button>
+                            <button  onClick={this.toggleMenu}>Menu</button>
+                            
+                        </div>)
+                    }
+
             </header>
         );
     }
