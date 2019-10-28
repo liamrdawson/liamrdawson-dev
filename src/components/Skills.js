a1694f145a5f217b0e5ca35dd851cb93ebe98f6b
Liam Dawson <liamrdawson@gmail.com>
2019-10-28 23:48:07 +0000

    refactor skills section to avoid unnecessary class and state use

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import SvgReact from '../icons/React';
 import SvgJs from '../icons/Js';
 import SvgSass from '../icons/Sass';
@@ -12,111 +12,96 @@ import SvgWebpack from '../icons/Webpack';
 import SvgExpress from '../icons/Express';
 
 
-class Skills extends Component {
+const Skills = () =>  {
+    const skills = [
+        {   
+            class: 'react',
+            area: 'front end',
+            iconModule: <SvgReact/>
+        },
+        {   
+            class: 'javascript',
+            area: 'front end',
+            iconModule: <SvgJs/>
+        },
+        { 
+            class: 'sass',
+            area: 'front end',
+            iconModule: <SvgSass/>
+        },
+        { 
+            class: 'css3',
+            area: 'front end',
+            iconModule: <SvgCss3/>
+        },
+        { 
+            class: 'html5',
+            area: 'front end',
+            iconModule: <SvgHtml5/>
+        },
+        { 
+            class: 'bootstrap',
+            area: 'front end',
+            iconModule: <SvgBootstrap/>
+        },
+        { 
+            class: 'webpack',
+            area: 'tooling',
+            iconModule: <SvgWebpack/>
+        },
+        { 
+            class: 'babel',
+            area: 'tooling',
+            iconModule: <SvgBabel/>
+        },
+        { 
+            class: 'git',
+            area: 'tooling',
+            iconModule: <SvgGit/>
+        },
+        { 
+            class: 'node',
+            area: 'back end',
+            iconModule: <SvgNode />
+        },
+        { 
+            class: 'express',
+            area: 'back end',
+            iconModule: <SvgExpress/>
+        },
+    ];
 
-    state = {
-        skillSet: [
-            {   
-                    class: 'react',
-                    area: 'front end',
-                    iconModule: <SvgReact/>
-            },
-            {   
-                    class: 'javascript',
-                    area: 'front end',
-                    iconModule: <SvgJs/>
-            },
-            { 
-                    class: 'sass',
-                    area: 'front end',
-                    iconModule: <SvgSass/>
-            },
-            { 
-                    class: 'css3',
-                    area: 'front end',
-                    iconModule: <SvgCss3/>
-            },
-            { 
-                    class: 'html5',
-                    area: 'front end',
-                    iconModule: <SvgHtml5/>
-            },
-            { 
-                class: 'bootstrap',
-                area: 'front end',
-                iconModule: <SvgBootstrap/>
-            },
-            { 
-                class: 'webpack',
-                area: 'tooling',
-                iconModule: <SvgWebpack/>
-            },
-            { 
-                class: 'babel',
-                area: 'tooling',
-                iconModule: <SvgBabel/>
-            },
-            { 
-                class: 'git',
-                area: 'tooling',
-                iconModule: <SvgGit/>
-            },
-            { 
-                class: 'node',
-                area: 'back end',
-                iconModule: <SvgNode />
-            },
-            { 
-                class: 'express',
-                area: 'back end',
-                iconModule: <SvgExpress/>
-            },
-        ]
-    };
-
-
-    render () {
-
-        const {
-            skill,
-            index
-        } = this.props;
-        console.log(`${skill} rendered. Index is ${index}`);
-
-        return (
-            <section className="skills-section">
-                <h2>Skills</h2>
-                <span>I love developing and learning about the latest technologies. Here are the technologies I use most.</span>
-                <h4>Front End</h4>
-                <div className="skills front-end">
-                    {this.state.skillSet.filter(skill => skill.area === 'front end').map( (skill) => 
-                        <div className={`skill-${skill.class} skill`}>
-                            {skill.iconModule}
-                        </div>
-                        
-                    )}
-                </div>
-                <h4>Back End</h4>
-                <div className="skills back-end">
-                    {this.state.skillSet.filter(skill => skill.area === 'back end').map( (skill) => 
-                        <div className={`skill-${skill.class} skill`}>
-                            {skill.iconModule}
-                        </div>
-                        
-                    )}
-                </div>
-                <h4>Tooling</h4>
-                <div className="skills tooling">
-                    {this.state.skillSet.filter(skill => skill.area === 'tooling').map( (skill) => 
-                        <div className={`skill-${skill.class} skill`}>
-                           {skill.iconModule}  
-                        </div>
-                        
-                    )}
-                </div>
-            </section>
-        );
-    }
+    return (
+        <section className="skills-section">
+            <h2>Skills</h2>
+            <span>I love developing and learning about the latest technologies. Here are the technologies I use most.</span>
+            <h4>Front End</h4>
+            <div className="skills front-end">
+                {skills.filter(skill => skill.area === 'front end').map( (skill) => 
+                    <div className={`skill-${skill.class} skill`}>
+                        {skill.iconModule}
+                    </div>
+                )}
+            </div>
+            <h4>Back End</h4>
+            <div className="skills back-end">
+                {skills.filter(skill => skill.area === 'back end').map( (skill) => 
+                    <div className={`skill-${skill.class} skill`}>
+                        {skill.iconModule}
+                    </div>
+                )}
+            </div>
+            <h4>Tooling</h4>
+            <div className="skills tooling">
+                {skills.filter(skill => skill.area === 'tooling').map( (skill) => 
+                    <div className={`skill-${skill.class} skill`}>
+                        {skill.iconModule}  
+                    </div>
+                    
+                )}
+            </div>
+        </section>
+    );
 }
 
 export default Skills;
