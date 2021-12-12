e26747ec32b34332d064f352b49c5beeb3e849cf
liamrdawson <liamrdawson@gmail.com>
2021-12-12 19:09:06 +0000

    Add prettier.

--- components/atoms/Link/index.tsx
+++ components/atoms/Link/index.tsx
@@ -1,20 +1,18 @@
 import Link from 'next/link'
 
 interface LinkAtomDataTypes {
-        path: string
-        name: string
+  path: string
+  name: string
 }
 
-const LinkAtom = ({path, name}: LinkAtomDataTypes) => {
-    return (
-        <Link href={path}>
-            <a>
-                <span>
-                    {name}
-                </span>
-            </a>
-        </Link> 
-    )
+const LinkAtom = ({ path, name }: LinkAtomDataTypes) => {
+  return (
+    <Link href={path}>
+      <a>
+        <span>{name}</span>
+      </a>
+    </Link>
+  )
 }
 
 export default LinkAtom
