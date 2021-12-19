a46dccd413fa79594c49ebe47bfc19bae4db686e
liamrdawson <liamrdawson@gmail.com>
2021-12-19 18:16:23 +0000

    working dynamic Heading component.

--- components/atoms/Heading/index.tsx
+++ components/atoms/Heading/index.tsx
@@ -1,24 +1,48 @@
-import { SerializedStyles, useTheme } from '@emotion/react'
+import { SerializedStyles, CSSObject, css } from '@emotion/react'
 import React from 'react'
-import { css } from '@emotion/react'
+import { theme, ITheme } from '../../Theme/theme'
 
 type HeadingProps = {
-  tag: keyof JSX.IntrinsicElements
+  tag: keyof IHeading
   children: string
   css?: SerializedStyles
 }
 
-const style = {
-  border: 'solid red 1px',
+const base = (headingTheme: ITheme = theme): SerializedStyles =>
+  css({
+    fontFamily: headingTheme.typography.secondaryFont,
+    textTransform: 'uppercase',
+  })
+
+interface IHeading {
+  h1: {
+    color: string
+  }
+  h2: {
+    color: string
+  }
+}
+
+const componentStyles = (element: keyof IHeading): CSSObject => {
+  const style = {
+    h1: {
+      color: 'red',
+    },
+    h2: {
+      color: 'blue',
+    },
+  }
+  return style[element]
 }
 
 export const Heading: React.FunctionComponent<HeadingProps & React.HTMLAttributes<HTMLOrSVGElement>> = ({
-  tag: CustomHeading = 'h1',
+  tag,
   children,
 }) => {
+  const CustomHeading = tag
   return (
     <>
-      <CustomHeading css={style}> {children} </CustomHeading>
+      <CustomHeading css={[base(), componentStyles(tag)]}> {children} </CustomHeading>
     </>
   )
 }
