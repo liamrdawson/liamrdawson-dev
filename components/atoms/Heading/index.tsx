fa2b4dc3ec0e0a134d93a53b3eb4ef9d7ea10a8d
liamrdawson <liamrdawson@gmail.com>
2021-12-19 18:24:18 +0000

    add notes to Heading.tsx.

--- components/atoms/Heading/index.tsx
+++ components/atoms/Heading/index.tsx
@@ -2,6 +2,10 @@ import { SerializedStyles, CSSObject, css } from '@emotion/react'
 import React from 'react'
 import { theme, ITheme } from '../../Theme/theme'
 
+// ?    Is there a better way of doing this?
+// ?    It kind of feels like assigning tag as a key of IHeading is misrepresenting what it is, as at the end of the day I'm rendering out a component.
+// ?    That being said, we are typing Heading as a React Functional Component, I'll need to read more into this to be sure.
+
 type HeadingProps = {
   tag: keyof IHeading
   children: string
