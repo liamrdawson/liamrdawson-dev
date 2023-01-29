5d61948a33995f7825c5ce013fef508b10d929d7
liamdawson <liamrdawson@gmail.com>
2023-01-29 22:16:08 +0000

    Update HeroPostPreview.

--- _posts/super.md
+++ _posts/super.md
@@ -1,6 +1,6 @@
 ---
 title: 'Super'
-excerpt: "The super keyword is used to access properties on an object literal or class's [[Prototype]], or invoke a superclass's constructor."
+excerpt: "The super keyword is used to access properties on an object literal or class's Prototype, or invoke a superclass's constructor."
 coverImage: 'https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80'
 date: '2022-11-22T22:17:46.950Z'
 author:
--- components/molecules/HeroPostPreview/index.tsx
+++ components/molecules/HeroPostPreview/index.tsx
@@ -1,31 +1,56 @@
 import Link from 'next/link'
 import { Heading } from '../../atoms/Heading'
 import styled from '@emotion/styled'
-import { css } from '@emotion/react'
 import type Post from '../../../types/post'
 import Image from 'next/image'
+import { css, useTheme } from '@emotion/react'
+import { ITheme } from '../../Theme/theme'
 
 type HeroPostInput = {
   post: Post
 }
+
+const Article = styled.article`
+  border: solid ${(props) => props.theme.colour.tertiary} 3px;
+`
 
 const ImageContainer = styled.div`
   height: 400px;
   position: relative;
 `
 
-const HeroPostPreview = ({ post }: HeroPostInput) => (
-  <article>
-    <Link as={`/blog/${post.slug}`} href="blog/[slug]" passHref>
-      <a>
-        <ImageContainer>
-          <Image src={post.coverImage} layout="fill" objectPosition={'0% 40%'} objectFit="cover" alt="" />
-        </ImageContainer>
-        <Heading tag={'h3'}>{post.title}</Heading>
-        <p>{post.excerpt}</p>
-      </a>
-    </Link>
-  </article>
-)
+const StyledLink = styled.a`
+  color: ${(props) => props.theme.colour.tertiary};
+  text-decoration: none;
+  outline: none;
+`
+
+const headingStyles = (theme: ITheme) => css`
+  color: ${theme.colour.secondary};
+  background: ${theme.colour.tertiary};
+`
+
+/**
+ * TODO: A HeroPostPreview is a post preview with an image on top. Split this out into its own PostPreview component.
+ */
+
+const HeroPostPreview = ({ post }: HeroPostInput) => {
+  const theme = useTheme()
+  return (
+    <Article>
+      <Link as={`/blog/${post.slug}`} href="blog/[slug]" passHref>
+        <StyledLink theme={theme}>
+          <ImageContainer>
+            <Image src={post.coverImage} layout="fill" objectPosition={'0% 40%'} objectFit="cover" alt="" />
+          </ImageContainer>
+          <Heading css={headingStyles(theme)} tag={'h3'}>
+            {post.title}
+          </Heading>
+          <p>{post.excerpt}</p>
+        </StyledLink>
+      </Link>
+    </Article>
+  )
+}
 
 export default HeroPostPreview
