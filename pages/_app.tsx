d2b72bc9f219a9174f26c10016fcdf6d6f34aff1
liamrdawson <liamrdawson@gmail.com>
2023-03-15 20:24:54 +0000

    Lint _app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@emotion/react'
-import { theme } from '../components/Theme/theme'
 import { AppProps } from 'next/app'
+import { theme } from '../components/Theme/theme'
 import Layout from '../components/templates/Layout'
 import { globalStyles } from '../shared/globalStyles'
 
