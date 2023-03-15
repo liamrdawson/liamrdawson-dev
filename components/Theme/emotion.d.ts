c1b02a2bbe3312296c9b6949cbea682182e32dff
liamrdawson <liamrdawson@gmail.com>
2023-03-15 19:58:03 +0000

    Lint emotion.d.ts.

--- components/Theme/emotion.d.ts
+++ components/Theme/emotion.d.ts
@@ -1,5 +1,6 @@
-import '@emotion/react'
 import { ITheme } from './theme' // Import type from above file
+import '@emotion/react'
+
 declare module '@emotion/react' {
   export interface Theme extends ITheme {} // extends the global DefaultTheme with our ThemeType.
 }
