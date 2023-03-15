bfba745cf2b7032afe9ae6654bb79886f70d0f55
liamrdawson <liamrdawson@gmail.com>
2023-03-15 19:53:57 +0000

    Lint Heading.

--- components/atoms/Heading/index.tsx
+++ components/atoms/Heading/index.tsx
@@ -1,22 +1,21 @@
-import { SerializedStyles, css, useTheme } from '@emotion/react'
+import { css, useTheme } from '@emotion/react'
 import React from 'react'
 import useHasMounted from '../../../utils/setHasMountedHook'
-import { ITheme, theme } from '../../Theme/theme'
+import { ITheme } from '../../Theme/theme'
 
 // ?    Is there a better way of doing this?
 // ?    It kind of feels like assigning tag as a key of IHeading is misrepresenting what it is, as at the end of the day I'm rendering out a component.
 // ?    That being said, we are typing Heading as a React Functional Component, I'll need to read more into this to be sure.
 
 type HeadingProps = {
-  tag?: Heading
+  tag?: HeadingLevel
   children: React.ReactNode
-  css?: SerializedStyles
   className?: string
 }
 
-type Heading = 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
 
-const getHeadingStyles = (tag: Heading, theme: ITheme) => {
+const getHeadingStyles = (tag: HeadingLevel, theme: ITheme) => {
   const base = css`
     text-transform: uppercase;
     color: ${theme.colour.text};
