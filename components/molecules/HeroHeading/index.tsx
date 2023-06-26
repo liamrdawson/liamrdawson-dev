33c3e093a8361606128993f00209c0c4120746a8
liamdawson <liamrdawson@gmail.com>
2023-06-26 23:07:25 +0100

    Remove redundant Heading molecule.
    Now being used as moduled css.

--- components/molecules/HeroHeading/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { Heading } from '../../atoms/Heading'
-
-const HeroHeading = styled(Heading)`
-  color: var(--color-text-inverse-base);
-  text-align: center;
-`
-
-export default HeroHeading
