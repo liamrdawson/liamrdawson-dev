feb8e26f2e01dce117a14fc3e072717577f7f5ab
liamdawson <liamrdawson@gmail.com>
2023-07-02 19:52:28 +0100

    Animate underline.

--- components/molecules/Nav/Nav.tsx
+++ components/molecules/Nav/Nav.tsx
@@ -1,3 +1,8 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { motion } from 'framer-motion'
+
 import LinkAtom from '@/components/atoms/Link/Link'
 import style from './nav.module.css'
 
@@ -10,16 +15,45 @@ interface Props {
   navLinks: INavLink[]
 }
 
-const Nav = ({ navLinks }: Props) => (
-  <nav className={style.nav}>
-    <ul>
-      {navLinks.map((navLink) => (
-        <li key={`navLink-${navLinks.indexOf(navLink)}`}>
-          <LinkAtom path={navLink.path} name={navLink.name} />
-        </li>
-      ))}
-    </ul>
-  </nav>
-)
+function isActiveLink(currentPathname: string, linkName: string) {
+  return currentPathname === linkName
+}
 
+const hoverLinkMotion = {
+  rest: {
+    x: '-101%',
+  },
+  hover: {
+    x: '0%',
+  },
+}
+
+const Nav = ({ navLinks }: Props) => {
+  const pathName = usePathname()
+
+  return (
+    <nav className={style.nav}>
+      <ul>
+        {navLinks.map((navLink) => (
+          <motion.li
+            key={`navLink-${navLinks.indexOf(navLink)}`}
+            data-active={isActiveLink(pathName, navLink.path)}
+            className={style.nav}
+            initial="rest"
+            whileHover="hover"
+          >
+            <span className={style['link-wrapper']}>
+              <LinkAtom path={navLink.path} name={navLink.name} />
+              <motion.div
+                transition={{ type: 'spring', duration: 0.25, bounce: 0 }}
+                variants={hoverLinkMotion}
+                className={style.underline}
+              />
+            </span>
+          </motion.li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
 export default Nav
